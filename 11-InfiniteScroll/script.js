9c385f3a2e8d9835d9fd86041f76d5fc97c66c73
Steve Poll <stevepoll@hotmail.com>
2020-06-25 17:10:41 -0400

    Completed Infinite Scroll

---
+++ 11-InfiniteScroll/script.js
@@ -0,0 +1,73 @@
+const postsContainer = document.getElementById('posts-container')
+const loading = document.querySelector('.loader')
+const filter = document.getElementById('filter')
+
+let limit = 5
+let page = 1
+
+// Fetch posts from API
+async function getPosts() {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`)
+
+  const data = await res.json()
+
+  return data
+}
+
+// Show posts in DOM
+async function showPosts(posts = null) {
+  if (!posts) {
+    posts = await getPosts()
+  }
+  posts.forEach(post => {
+    const postEl = document.createElement('div')
+    postEl.classList.add('post')
+    postEl.innerHTML = `
+      <div class="number">${post.id}</div>
+      <div class="post-info">
+        <h2 class="post-title">${post.title}</h2>
+        <p class="post-body">${post.body}</p>
+      </div>
+    `
+    postsContainer.appendChild(postEl)
+  });
+}
+
+// Show loading and fetch more posts
+function showLoading() {
+  loading.classList.add('show')
+  
+  setTimeout(() => {
+    loading.classList.remove('show')
+
+    setTimeout(() => {
+      page++
+      showPosts()
+    }, 300);
+  }, 1000);
+}
+
+// Show initial posts
+showPosts()
+
+window.addEventListener('scroll', () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement
+
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    showLoading()
+  }
+})
+
+filter.addEventListener('input', () => {
+  const term = filter.value.toUpperCase()
+  const allPosts = document.querySelectorAll('.post')
+  allPosts.forEach((post) => {
+    const title = post.querySelector('.post-title').textContent.toUpperCase()
+    const body = post.querySelector('.post-body').textContent.toUpperCase()
+    if (title.includes(term) || body.includes(term)) {
+      post.style.display = 'flex'
+    } else {
+      post.style.display = 'none'
+    }
+  })
+})
